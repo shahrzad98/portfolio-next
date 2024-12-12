59704bb60f58f776680833f11b958dcd54acefe5
shahrzad98 <shahrzad.gheidi@gmail.com>
2024-12-12 23:41:02 +0330

    [Add] next production configs

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,12 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {};
+const isProduction = process.env.NODE_ENV === 'production';
+const nextConfig: NextConfig = {
+  basePath: isProduction ? '/portfolio-next' : '',
+  assetPrefix: isProduction ? '/portfolio-next/' : '',
+  images: {
+    unoptimized: true, // Necessary for static export to work with Next.js
+  },
+};
 
 export default nextConfig;
