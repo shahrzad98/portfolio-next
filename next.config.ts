11ec6fd2eab3aa2787d646ba8482d7332cc46c5c
shahrzad98 <shahrzad.gheidi@gmail.com>
2025-03-14 23:17:42 +0330

    [Add] auto deploy

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from 'next';
 const isProduction = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
   basePath: isProduction ? process.env.NEXT_PUBLIC_BASE_PATH || '' : '',
-  assetPrefix: isProduction ? process.env.NEXT_PUBLIC_BASE_PATH || '' : '',
+  assetPrefix: isProduction ? '/portfolio-next/' : '',
   images: {
     unoptimized: true, // Necessary for static export to work with Next.js
   },
